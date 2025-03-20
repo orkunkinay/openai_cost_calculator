16d57680bea4ffb79c4d6df83cd58bbe4e3cfc8a
Orkun Kinay <orkunkinay@sabanciuniv.edu>
2025-03-20 13:06:36 +0300

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openai_cost_calculator",
-    version="0.1.0",
+    version="0.1.1",
     description="A library to estimate OpenAI API costs based on token usage.",
     author="Orkun Kınay, Murat Barkın Kınay",
     author_email="orkunkinay@sabanciuniv.edu",
